1d79b1a306d6423fdc7f042072ad92f71bacde27
JackMD <jackmtaylor.jmt@gmail.com>
2019-04-24 13:27:23 +0500

    Close #3

--- BasicAddon.php
+++ BasicAddon.php
@@ -4,8 +4,9 @@ declare(strict_types = 1);
 /**
  * @name BasicAddon
  * @version 1.0.0
- * @main JackMD\ScoreHud\Addons\BasicAddon
+ * @main    JackMD\ScoreHud\Addons\BasicAddon
  */
+
 namespace JackMD\ScoreHud\Addons
 {
 
@@ -20,24 +21,25 @@ namespace JackMD\ScoreHud\Addons
 		 */
 		public function getProcessedTags(Player $player): array{
 			return [
-				"{name}"              => $player->getName(),
-				"{online}"            => count($player->getServer()->getOnlinePlayers()),
-				"{max_online}"        => $player->getServer()->getMaxPlayers(),
-				"{item_name}"         => $player->getInventory()->getItemInHand()->getName(),
-				"{item_id}"           => $player->getInventory()->getItemInHand()->getId(),
-				"{item_meta}"         => $player->getInventory()->getItemInHand()->getDamage(),
-				"{item_count}"        => $player->getInventory()->getItemInHand()->getCount(),
-				"{x}"                 => intval($player->getX()),
-				"{y}"                 => intval($player->getY()),
-				"{z}"                 => intval($player->getZ()),
-				"{load}"              => $player->getServer()->getTickUsage(),
-				"{tps}"               => $player->getServer()->getTicksPerSecond(),
-				"{level_name}"        => $player->getLevel()->getName(),
-				"{level_folder_name}" => $player->getLevel()->getFolderName(),
-				"{ip}"                => $player->getAddress(),
-				"{ping}"              => $player->getPing(),
-				"{time}"              => date($this->getScoreHud()->getConfig()->get("time-format")),
-				"{date}"              => date($this->getScoreHud()->getConfig()->get("date-format")),
+				"{name}"               => $player->getName(),
+				"{online}"             => count($player->getServer()->getOnlinePlayers()),
+				"{max_online}"         => $player->getServer()->getMaxPlayers(),
+				"{item_name}"          => $player->getInventory()->getItemInHand()->getName(),
+				"{item_id}"            => $player->getInventory()->getItemInHand()->getId(),
+				"{item_meta}"          => $player->getInventory()->getItemInHand()->getDamage(),
+				"{item_count}"         => $player->getInventory()->getItemInHand()->getCount(),
+				"{x}"                  => intval($player->getX()),
+				"{y}"                  => intval($player->getY()),
+				"{z}"                  => intval($player->getZ()),
+				"{load}"               => $player->getServer()->getTickUsage(),
+				"{tps}"                => $player->getServer()->getTicksPerSecond(),
+				"{level_name}"         => $player->getLevel()->getName(),
+				"{level_folder_name}"  => $player->getLevel()->getFolderName(),
+				"{ip}"                 => $player->getAddress(),
+				"{ping}"               => $player->getPing(),
+				"{time}"               => date($this->getScoreHud()->getConfig()->get("time-format")),
+				"{date}"               => date($this->getScoreHud()->getConfig()->get("date-format")),
+				"{world_player_count}" => count($player->getLevel()->getPlayers())
 			];
 		}
 	}
